be9f043cf9933adf98b7063a5f4dbf48e549adda
guisouco <guilherme.souzacolacio@gmail.com>
2021-10-20 23:31:19 -0300

    Arquivos Estaticos

--- pypro/settings.py
+++ pypro/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#Configuração de ambiente de desenvolvimento
+# Configuração de ambiente de desenvolvimento
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR / 'mediafiles')
