81bd9980b86f046537c4959deb998cfb4b74a79b
guisouco <guilherme.souzacolacio@gmail.com>
2021-11-29 17:16:38 -0300

    Concertado erros de HTML

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -13,9 +13,9 @@ def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_titulo_video(resp, video):
-    assert_contains(resp, video.titulo)
+def test_titulo_video(resp):
+    assert_contains(resp, 'Video Aperitivo: Motivacao')
 
 
-def test_conteudo_video(resp, video):
-    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}"')
+def test_conteudo_video(resp):
+    assert_contains(resp, 'f<iframe src="https://player.vimeo.com/video/251224475"')
